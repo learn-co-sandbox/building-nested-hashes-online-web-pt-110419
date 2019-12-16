354709694a1b2a8e9a3e1d70c18976fc4d829afe
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-16 22:18:12 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -28,8 +28,6 @@ def bonus
  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   #Don't touch the following line! The `bonus` method must return our newly modified
- 
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+  epic tragedy hash
   epic_tragedy
 end
